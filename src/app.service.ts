8ef471671f8d6e2e9ae9cb9975cbc40c000abea3
Ran Har-Shuv <ran.harshuv@gmail.com>
2019-03-09 23:19:18 +0200

    define defualt page

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getDefaultPage(): string {
+    return `<h1>Wallet Tracker API</h1>
+    `;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-  console.log('about to create app object');
   const app = await NestFactory.create(AppModule);
-  console.log('app object created');
-  console.log('going to listen on port' + port);
   await app.listen(port);
-  console.log('done listening on port' + port);
 }
 bootstrap();
