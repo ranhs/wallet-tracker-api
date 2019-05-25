c1ad398422a782577f989576b7805783ec3415c5
Ran Har-Shuv <ran.harshuv@gmail.com>
2019-05-25 22:21:58 +0300

    add transaction service and routes

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, HttpException, HttpStatus, Req } from "@nestjs/common";
+import { Controller, Post, Body, HttpException, HttpStatus, Req, Get, Param, Patch, Delete } from "@nestjs/common";
 import { TransactionsService } from "./transaction.service";
 import { ITransaction } from "src/interfaces/transaction.interface";
 import { RequestEx } from "src/middleware/authentication";
@@ -20,5 +20,64 @@ export class TransactionController {
             throw new HttpException(e, HttpStatus.BAD_REQUEST)
         }
     }
+
+    @Get('')
+    async getAllTransactions(@Req() request:RequestEx): Promise<ITransaction[]> {
+        // if ( !request.user.admin ) {
+        //     throw new HttpException('only admin can see all users', HttpStatus.UNAUTHORIZED )
+        // }
+        return await this.transactionsService.getAllTransactions(request.user._id)
+    }
+
+    @Get(':id')
+    async getTransaction(@Req() request:RequestEx, @Param() params : {id:string}): Promise<ITransaction> {
+        try {
+            let transaction = await this.transactionsService.getTransactionById(params.id, request.user._id)
+            if ( ! transaction ) {
+                throw new HttpException('transaction not found', HttpStatus.NOT_FOUND)
+            }
+            return transaction
+        } catch (e) {
+            if ( e.status )
+                throw e
+            throw new HttpException( e, HttpStatus.BAD_REQUEST )
+        }
+    }
+
+    @Patch(':id')
+    async updateTransaction(@Req() request:RequestEx, @Body() updatedTransaction: ITransaction, @Param() params : {id:string}): Promise<ITransaction> {
+        try {
+            const transactionToUpdate = await this.transactionsService.getTransactionById(params.id, request.user._id)
+            if ( !transactionToUpdate ) {
+                throw new HttpException('user not found', HttpStatus.NOT_FOUND)
+            }
+            let {transaction, err} = await this.transactionsService.updateTransaction(transactionToUpdate, updatedTransaction, request.user._id)
+            if ( err ) {
+                throw err.message
+            } else {
+                return transaction
+            }
+        } catch (e) {
+            if ( e.status )
+                throw e
+            throw new HttpException( e, HttpStatus.BAD_REQUEST )
+        }
+    }
+
+    @Delete(':id')
+    async deleteTransaction(@Req() request:RequestEx, @Param() params : {id:string}): Promise<void> {
+        try {
+            const transactionToDelete = await this.transactionsService.getTransactionById(params.id, request.user._id)
+            if ( !transactionToDelete ) {
+                throw new HttpException('user not found', HttpStatus.NOT_FOUND)
+            }
+            return this.transactionsService.deleteTransaction(transactionToDelete, request.user._id)
+        } catch (e) {
+            if ( e.status )
+                throw e
+            throw new HttpException( '', HttpStatus.INTERNAL_SERVER_ERROR )
+        }
+    }
+
 
 }
--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,5 +1,5 @@
 import { ITransaction } from "src/interfaces/transaction.interface";
-import { Transaction } from "src/models/transaction.model";
+import { Transaction, ITransactionModel } from "src/models/transaction.model";
 import { ObjectId } from "mongodb";
 
 export class TransactionsService {
@@ -10,4 +10,46 @@ export class TransactionsService {
         let savedTransaction: Transaction = await transaction.save()
         return savedTransaction
     }
+
+    async getAllTransactions(owner_id: ObjectId): Promise<ITransaction[]> {
+        let transactions = await Transaction.find({owner: owner_id})
+        return transactions
+    }
+
+    async getTransactionById(id: string, owner_id: ObjectId): Promise<ITransaction> {
+        try {
+            let transaction = await Transaction.findById(id)
+            if ( !transaction || transaction.owner.toHexString() !== owner_id.toHexString() ) return null
+            return transaction
+        } catch (e) {
+            return null
+        }
+    }
+
+    async updateTransaction(transaction: ITransaction, updatedTransaction: ITransaction, owner_id: ObjectId) : Promise<{transaction?: ITransaction, err?: Error}> {
+        if ( transaction.owner.toHexString() !== owner_id.toHexString() ) {
+            return { err: Error('Invalid transaction!') }
+        }
+        const updates = Object.keys(updatedTransaction)
+        let allowedUpdates = ['date', 'description', 'value']
+        const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    
+        if (!isValidOperation) {
+            return { err: Error('Invalid updates!') }
+        }
+
+        updates.forEach((update) => transaction[update] = updatedTransaction[update])
+        transaction.updateTime = Date.now()
+        await (<ITransactionModel>transaction).save()
+        return { transaction }
+    }
+
+    async deleteTransaction(transaction: ITransaction, owner_id: ObjectId): Promise<void> {
+        if ( transaction.owner.toHexString() !== owner_id.toHexString() ) {
+            throw 'Invalid transaction!';
+        }
+        await (<ITransactionModel>transaction).remove()
+    }
+
+
 }
