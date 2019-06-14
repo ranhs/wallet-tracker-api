89698098a03a279058e73322c84e902c3c2db83e
Ran Har-Shuv <ran.harshuv@gmail.com>
2019-06-14 20:45:16 +0300

    log

--- src/mongoosedb.service.ts
+++ src/mongoosedb.service.ts
@@ -3,7 +3,7 @@ import {connect} from 'mongoose'
 export class MongooseDbService {
     constructor() {
         let uri: string = process.env.MONGODB_URI || 'mongodb://localhost:27017/WalletTracker'
-        console.log(`***************************{{uri}}**************************`)
+        console.log(`***************************${uri}**************************`)
         connect(uri, {
             useNewUrlParser: true,
             useCreateIndex: true
